64ca0f06d268e0d41e4a2e0d0863c37faedf54f7
Jose Pablo Alberto Andreotti <albertoandreotti@gmail.com>
2013-12-09 15:15:59 -0300

    Some changes for speaker ident.

---
+++ data/generate_sp.m
@@ -0,0 +1,96 @@
+%this script will crawl the MFCC data from VoxForge to generate the en_de_it.mat file 
+%containing MFCCs for the three languages English, Deutsch and Italian.
+%this file assumes VoiceBox is in your Octave/Matlab's path.
+
+en_endpoint = 'http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Original/44.1kHz_16bit/';
+de_endpoint = 'http://www.repository.voxforge1.org/downloads/de/Trunk/Audio/Original/48kHz_16bit/';
+it_endpoint = 'http://www.repository.voxforge1.org/downloads/it/Trunk/Audio/Original/48kHz_16bit/';
+endpoint = de_endpoint;
+%with data so far, 700 -> it, 450 -> de, 90 -> en
+%mean, std: (0.85, 0.93)-> it,(0.95,0.98)-> de , (1.16, 1.35) -> en
+limit = 450;
+
+%flist = urlread(endpoint);
+flist = 'knotyouraveragejo-20080426-adv.tgz';
+
+[s,e] = regexp(flist, ">([a-zA-Z0-9]*-[a-zA-Z0-9]*)+\.tgz<");
+%truncate the amount of data to be crawled
+s = s(1:min(limit, size(s,2)));
+e = e(1:min(limit, size(s,2)));
+
+confirm_recursive_rmdir(0)
+filename = "de.mat";
+    
+function data = fetch_data(flist, endpoint, anfang, ende, id)
+    
+    Tw = 25;           % analysis frame duration (ms)
+    Ts = 8.33;         % analysis frame shift (ms)
+    alpha = 0.97;      % preemphasis coefficient
+    R = [20 3500];    % frequency range to consider
+    M = 39;            % number of filterbank channels 
+    C = 39;            % number of cepstral coefficients
+    L = 55;            % cepstral sine lifter parameter
+       
+    % hamming window (see Eq. (5.2) on p.73 of [1])
+    hamming = @(N)(0.54-0.46*cos(2*pi*[0:N-1].'/(N-1)));
+
+    %print(int2str(id));  
+    %at each step fetch a file from the corpus
+    currfile = flist(anfang + 1: ende - 1);
+    currdir = strcat("temp", int2str(id));
+    cd('/home/ubuntu/DeepLearnToolbox/data'); 
+    mkdir(currdir);cd(currdir);
+    data = zeros(C, 1);
+    status = urlwrite(strcat(endpoint, currfile), currfile);
+ 	
+    read_size = 0;
+    %Unzip the mfc files to temp dir and add them to the dataset.
+    %TODO: only working in Linux?.
+    untar(currfile); cd(currfile(1:end-4)); 
+	
+    a = fileattrib ('wav');
+    b = fileattrib ('flac');
+    
+    if (a) 
+	    cd wav;
+	    mfcs = ls("*.wav");
+    elseif(b)
+	    cd flac;
+	    system('sh ~/DeepLearnToolbox/convToWav.sh')
+	    mfcs = ls("*.wav");
+    end
+    
+    for j=1:size(mfcs,1)
+        
+        % Read speech samples, sampling rate and precision from file
+        [speech, fs, nbits ] = wavread(strtrim(mfcs(j, :)));
+	       
+        % Feature extraction (feature vectors as columns)
+        [MFCCs, FBEs, frames ] = mfcc(normalizeMax(speech), fs, Tw, Ts, alpha, hamming, R, M, C, L );
+
+	 %Check that the computed values are healthy
+        if (sum(sum(isnan(FBEs)))==0 && max(max(FBEs))<70 && min(min(FBEs))>-70)
+            data = [data, FBEs];
+        end
+
+    end
+    cd ../../..
+    rmdir(currdir, "s");
+end
+
+%here goes what to put in the output when the function fails.
+function retcode = eh(error)
+    a = error
+    dbstack
+    %cd ../../..
+    retcode = zeros(26,1);	
+end
+
+mfccs = pararrayfun(numWorkers = 15,
+                    @(anfang, ende, id)fetch_data(flist, endpoint, anfang, ende, id), %currying with anonym funct
+                    s, e, 1:size(s,2), %parameters for the function
+                    "ErrorHandler" , @eh);
+
+read_size = size(mfccs)
+rmdir('temp*', "s");
+save("-mat4-binary", filename, "mfccs");
---
+++ tests/test_speaker_CNN.m
@@ -0,0 +1,100 @@
+function net = test_speaker_CNN
+
+%Set up dataset
+winsize = 607;
+mfccnum = 39;
+
+%Which fraction of the whole dataset will be used for training?
+train_fraction = 0.97;
+
+files = ["en.mat"; "sp.mat"];
+usable = intmax;
+
+%Clean memory often while preparing the dataset
+for i=1:size(files,1)
+    load(files(i, :));
+	label = zeros(2, 1); label(i, 1)=1;
+    
+	if !exist('train_x')
+	    usable = min(usable, floor(size(mfccs, 2)/winsize));
+		%Reduce the amount of data according to memory available
+        usable = floor(0.75*usable);
+	    train_x = reshape(mfccs(:, 1:(usable*winsize)), mfccnum, winsize, []);
+        train_y = zeros(2, usable) + label;
+	    clear mfccs
+	else
+	    %Repeat speaker information until it is long as 'usable'
+		times = floor((usable*winsize)/size(mfccs,2));
+		mfccs = repmat(mfccs, 1, times);
+		reshaped = reshape(mfccs(:, 1:usable*winsize), mfccnum, winsize, []);
+	    clear mfccs	
+	    train_x = cat(3, train_x, reshaped);
+        clear reshaped
+	    train_y = cat(2, train_y, zeros(2, usable) + label);
+	end
+       clear mfccs	
+end
+
+%scaling/mean normalization 
+average = 0.98;	
+train_x = train_x .-average;
+train_x = train_x/8;
+
+
+%shuffle the training set, perhaps not necesary as it is done in the cnn
+n = rand(size(train_x, 3),1); 
+[nn index] = sort(n); 
+train_x = train_x(:,:,index); 
+train_y = train_y(:,index);
+
+%The rest of the dataset goes for testing
+e = size(train_x, 3);
+s = floor(e*train_fraction);
+
+test_x(:,:,1:(e-s+1)) = train_x(:,:,s:e);
+test_y(:, 1:(e-s+1)) = train_y(:, s:e);
+
+percentage_class1 = sum(train_y(1, :))/size(train_y,2)
+percentage_class2 = sum(train_y(2, :))/size(train_y,2)
+percentage_class3 = sum(train_y(3, :))/size(train_y,2)
+
+isNanX = sum(sum(sum(isnan(train_x))))
+isNanY = sum(sum(isnan(train_y)))
+
+DataSetSize = size(train_x, 3)
+
+pause
+
+
+if !isOctave() 
+  rng(0)
+end
+
+cnn.layers = {
+    struct('type', 'i') %input layer
+    struct('type', 'c', 'outputmaps', 12, 'kernelsize', 6) %convolution layer
+    struct('type', 's', 'xscale', 2, 'yscale', 2) %sub sampling layer
+    struct('type', 'c', 'outputmaps', 12, 'kernelsize', 6) %convolution layer
+    struct('type', 's', 'xscale', 2, 'yscale', 2) %subsampling layer
+    struct('type', 'c', 'outputmaps', 12, 'kernelsize', 6) %convolution layer
+    struct('type', 's', 'xscale', 1, 'yscale', 143) %subsampling layer
+};
+
+
+opts.alpha = .6;
+opts.batchsize = 2;
+opts.numepochs = 8;
+
+%Force the number of batches(totalbtch) to be a multiple of the batchsize
+totalbtch = floor((size(train_x,3)*train_fraction)/(opts.batchsize))*opts.batchsize;
+
+cnn = cnnsetup(cnn, train_x(:,:, 1:totalbtch), train_y(:,1:totalbtch));
+cnn = cnntrain(cnn, train_x(:,:, 1:totalbtch), train_y(:,1:totalbtch), opts);
+
+[er, bad] = cnntest(cnn, test_x, test_y);
+er
+%plot mean squared error
+%figure; plot(cnn.rL);
+net = cnn;
+er
+
